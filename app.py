a9ff108ccfceefd37ce970e64f5ad102ce6975cd
Ahmed Hussein <ahmedhussiien@gmail.com>
2020-08-13 05:33:22 +0200

    Add swagger API documentation + Add Debug mode

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 from serve import get_model_api 
+import argparse
 
 app = Flask(__name__)
+CORS(app)
 model_api = get_model_api()
+
+### swagger specific ###
+SWAGGER_URL = '/api/documentation'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "WideBot-Classifier-API"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 
 # Default route
 @app.route('/')
 def home():
-    return render_template('home.html')
+   return render_template('home.html')
 
 # HTTP Errors handlers
 @app.errorhandler(404)
@@ -27,8 +44,22 @@ def server_error(e):
 @app.route('/api', methods=['GET'])
 def api():
     output_data = model_api(request)[0]
-    response = jsonify(prediction = output_data)
+    response = jsonify(predictedClass = output_data)
     return response
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    PARSER = argparse.ArgumentParser(
+        description="WideBot-Classifier-API")
+
+    PARSER.add_argument('-d', '--debug', action='store_true',
+                        help="Use flask debug/dev mode with file change reloading")
+    ARGS = PARSER.parse_args()
+
+    if ARGS.debug:
+        print("Running in debug mode")
+        app.run(debug=True)
+
+    else:
+        app.run(debug=False)
